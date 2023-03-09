5a6bb0d303f00e4a254ffb5342fa43343818a89e
Yves <cauayves5@gmail.com>
2023-03-09 19:26:21 -0300

    filmes renderizados na tela

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,26 +1,28 @@
+import axios from "axios"
+import MovieContainer from "../../components/MovieContainer"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 
 export default function HomePage() {
+    const [imagens, setImagens] = useState([])
+
+    useEffect(() => {
+        const url = "https://mock-api.driven.com.br/api/v8/cineflex/movies"
+
+        const promise = axios.get(url)
+        promise.then((answer) => {
+            setImagens(answer.data)
+        })
+    }, [])
+
     return (
         <PageContainer>
             Selecione o filme
 
             <ListContainer>
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
+                {imagens.map((objects) => {
+                    return <MovieContainer key={objects.id} objects={objects} />
+                })}
             </ListContainer>
 
         </PageContainer>
@@ -44,18 +46,4 @@ const ListContainer = styled.div`
     flex-wrap: wrap;
     flex-direction: row;
     padding: 10px;
-`
-const MovieContainer = styled.div`
-    width: 145px;
-    height: 210px;
-    box-shadow: 0px 2px 4px 2px #0000001A;
-    border-radius: 3px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    img {
-        width: 130px;
-        height: 190px;
-    }
 `
