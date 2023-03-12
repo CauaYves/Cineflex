331d217886dea9feaa1469f8956a03c0a2cb31dd
Yves <cauayves5@gmail.com>
2023-03-12 10:35:33 -0300

    valores de assenstos recebidos e renderizados conforme reserva

--- src/components/Chairs.js
+++ src/components/Chairs.js
@@ -1,15 +1,16 @@
 import styled from "styled-components"
 
 export default function Chairs({chairs}){
- 
+
+    console.log(chairs)
+
     return(
-        chairs.map((i, index) => {
+        chairs.map((chair, index) => {
             return(
                 <SeatItem 
                     key={index}
-                    
+                    isAvailable={chair.isAvailable}
                 >
-
                     {[index]}
                 </ SeatItem>
             )
@@ -19,8 +20,17 @@ export default function Chairs({chairs}){
 }
 
 const SeatItem = styled.div`
-    border: 1px solid blue;         // Essa cor deve mudar
-    background-color: lightblue;    // Essa cor deve mudar
+
+    /* 
+    
+    green: #0E7D71 - #1AAE9E
+    yellow: #F7C52B - #FBE192
+    gray: #7B8B99 - #C3CFD9
+    */
+    background-color: ${(props) => props.isAvailable ? "#1AAE9E" : "#FBE192"};    
+    border-color: ${(props) => props.isAvailable ? "#1AAE9E" : "#FBE192"};             // Essa cor deve mudar
+
+    border: 1px solid;          
     height: 25px;
     width: 25px;
     border-radius: 25px;
